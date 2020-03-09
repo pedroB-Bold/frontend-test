ab736a103318cfa1243aa957d6653a63dcfbb1c9
Pedro Brosque <>
2020-03-09 14:15:14 +0000

    Added navigation to header

--- code/movie-app/src/Components/Header.js
+++ code/movie-app/src/Components/Header.js
@@ -1,20 +1,31 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 import './Header.css'
+
 const Header = (props) => (
-  <div className="header">
-    <img
-      className="logo"
-      src="https://talentportugal.com/wp-content/uploads/listing-uploads/logo/2019/06/4dUkCtgE.jpg"
-      alt="movieImg"
-    />
-    <form className="form" onSubmit={props.handleSubmit}>
-      <input
-        style={{ width: "100%" }}
-        value={props.value}
-        onChange={e => props.handleChange(e)}
+  <div className='headerContainer'>
+    <div className="header">
+      <img
+        className="logo"
+        src="https://talentportugal.com/wp-content/uploads/listing-uploads/logo/2019/06/4dUkCtgE.jpg"
+        alt="movieImg"
       />
-      <button>Add</button>
-    </form>
+      <form className="form" onSubmit={props.handleSubmit}>
+        <input
+          style={{ width: "100%" }}
+          value={props.value}
+          onChange={e => props.handleChange(e)}
+        />
+        <button>Add</button>
+      </form>
+    </div>
+    {props.navigateBack ? 
+        <Link to={props.navigateBack}>
+          <div id='moviePageHomeBtn'>HOME</div>
+        </Link>
+        :
+        null    
+    }
   </div>
 );
 export default Header;
